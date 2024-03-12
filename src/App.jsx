beacf56d5dc276620a820ea3b59f8366752613a7
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-03-12 12:14:32 -0300

    genera el square

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import './App.css'
+const turns = {
+  x: 'x',
+  o: 'o',
+}
 
+const Square = ({children, updateBoard,index})=>{
+  return(
+    <div className="square" key={index}>
+      {children}
+  </div>
+  )
+}
+
+const board = Array(9).fill(null)
 function App() {
  return (
+  <main className="board">
   <h1>Ta-Te-Ti</h1>
+  <section className="game">
+    {board.map((_ , index)=>{
+      return (
+        <Square
+         key={index}
+         index={index}
+        >
+          {index}
+        </Square>
+      )
+    })}
+  </section>
+  </main>
  )
 }
 
