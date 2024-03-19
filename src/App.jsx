a6b14cbde5997940ea83d88a9a7322ae29c5a91d
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-03-19 10:44:23 -0300

    App: crea función para saber si hay ganador

--- src/App.jsx
+++ src/App.jsx
@@ -5,58 +5,95 @@ const TURNS = {
   O: 'o',
 }
 
-const Square = ({children, isSelected, updateBoard,index})=>{
-  const className = `square ${isSelected ? 'is-selected': ''}`
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
 
-  const handleChange = ()=>{
+  const handleChange = () => {
     updateBoard(index)
   }
 
-  return(
-    <div onClick={handleChange} className={className} key={index}> 
+  return (
+    <div onClick={handleChange} className={className} key={index}>
       {children}
-  </div>
+    </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
-  console.log(board);
+  const [winnner, setWinner] = useState(null)
 
-  const updateBoard =(index)=>{
 
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    } 
+    return null; 
+  }
+
+
+
+
+
+  const updateBoard = (index) => {
+
+    if (board[index] || winnner) return
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+      alert(`Ha ganado ${newWinner}`)
+    }
   }
 
- return (
-  <main className="board">
-  <h1>Ta-Te-Ti</h1>
-  <section className="game">
-    {board.map((_ , index)=>{
-      return (
-        <Square
-         key={index}
-         index={index}
-         updateBoard={updateBoard}
-        >
-          {board[index]}
-        </Square>
-      )
-    })}
-  </section>
-  <section className="turn">
-    <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
-    <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
-  </section>
-  </main>
- )
+  return (
+    <main className="board">
+      <h1>Ta-Te-Ti</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+            >
+              {board[index]}
+            </Square>
+          )
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  )
 }
 
 export default App
